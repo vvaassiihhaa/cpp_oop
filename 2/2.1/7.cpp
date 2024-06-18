fc744cf19edf9220e426e9a6d8d1715b6d298c06
hisava <veretennikovalexey@gmail.com>
2024-06-18 17:05:54 +0300

    alex

--- 2/2.1/7.cpp
+++ 2/2.1/7.cpp
@@ -3,10 +3,15 @@
 class TriangleChecker {
     int a, b, c;
 
-public
-    int is_triangle() {
-    }
+public:
+    TriangleChecker(int a, int b, int c) : a(a), b(b), c(c) {}
 
+    // в этот метод ничего передавать не надо, потому что есть поля объекта
+    int is_triangle() {
+        if ( a <= 0 || b <= 0 || c <= 0 ) return 1;
+        if ( a + b <= c || a + c <= b || b + c <= a ) return 2;   
+        return 3; 
+    }                        // и они уже заполнены
 };
 
 int main(void)
@@ -15,8 +20,9 @@ int main(void)
     std::cin >> a >> b >> c;
 
     TriangleChecker * ptr_tr = new TriangleChecker( a, b, c );
+    std::cout << ptr_tr->is_triangle() << std::endl;
 
-    __ASSERT_TESTS__ // макроопределение для тестирования (не убирать и должно идти непосредственно перед return 0 или перед освобождением памяти)
+    // __ASSERT_TESTS__ // 
 
     delete ptr_tr;
 
